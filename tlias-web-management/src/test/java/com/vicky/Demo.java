7b79643bacfd943a334bc43711ec412abbf5fef3
vk666666 <123374103+vk666666@users.noreply.github.com>
2025-04-07 15:54:49 +0800

    Update Demo.java

--- tlias-web-management/src/test/java/com/vicky/Demo.java
+++ tlias-web-management/src/test/java/com/vicky/Demo.java
@@ -16,12 +16,12 @@ public class Demo {
         // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
         EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
         // 填写Bucket名称，例如examplebucket。
-        String bucketName = "vkvvkk";
+        String bucketName = "xxxxx";
         // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
         String objectName = "1.jpg";
         // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
         // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
-        String filePath= "/Users/xiong/Downloads/1.jpg";
+        String filePath= "/Users/xx'x/xxxxx/1.jpg";
         // 填写Bucket所在地域。以华东1（杭州）为例，Region填写为cn-hangzhou。
         String region = "cn-beijing";
         
